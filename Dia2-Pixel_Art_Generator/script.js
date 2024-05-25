feb2f6d1c6e559b8652a29e67cf7f93092f52ab8
Alcides Paz <alcidesnp@hotmail.com>
2024-05-24 21:38:01 -0300

    fix: Correcao Ids html, funcoes no js

--- Dia2-Pixel_Art_Generator/script.js
+++ Dia2-Pixel_Art_Generator/script.js
@@ -2,18 +2,18 @@ let container = document.querySelector(".container");
 let gridButton = document.getElementById("submit-grid");
 let clearGridButton = document.getElementById("clear-grid");
 let gridWidth = document.getElementById("width-range");
-let gridHeigth = document.getElementById("heigth-range");
+let gridHeight = document.getElementById("height-range");
 let colorButton = document.getElementById("color-input");
 let eraseBtn = document.getElementById("erase-btn");
 let paintBtn = document.getElementById("paint-btn");
 let widthValue = document.getElementById("width-value");
-let heigthValue = document.getElementById("heigth-value");
+let heightValue = document.getElementById("height-value");
 
 let events = {
     mouse: {
         down: "mousedown",
         move: "mousemove",
-        up: "touchend"
+        up: "mouseup"
     },
     touch: {
         down: "touchstart",
@@ -30,7 +30,7 @@ let erase = false;
 const isTouchDevice = () =>{
     try{
         document.createEvent("TouchEvent");
-        deviceType = "Touch";
+        deviceType = "touch";
         return true;
     } catch (e) {
         deviceType = "mouse";
@@ -43,14 +43,14 @@ isTouchDevice();
 gridButton.addEventListener("click", () => {
     container.innerHTML = "";
     let count = 0;
-    for(let i=0; i < gridHeigth.value; i++){
+    for(let i=0; i < gridHeight.value; i++){
         count +=2;
-        let div = document.createEvent("div");
+        let div = document.createElement("div");
         div.classList.add("gridRow");
 
         for(let j=0; j < gridWidth.value; j++) {
         count += 2;
-        let col = document.createEvent("div");
+        let col = document.createElement("div");
         col.classList.add("gridCol");
         col.setAttribute("id", `gridCol${count}`);
         col.addEventListener(events[deviceType].down, () =>{
@@ -66,7 +66,7 @@ gridButton.addEventListener("click", () => {
             let elementId = document.elementFromPoint(
                 !isTouchDevice() ? e.clientX : e.touches[0].clientX,
                 !isTouchDevice() ? e.clientY : e.touches[0].clientY
-            ), id;
+            ).id;
             checker(elementId);
         });
 
@@ -111,11 +111,11 @@ gridWidth.addEventListener("input", ()=>{
     widthValue.innerHTML = gridWidth.value < 9 ? `0${gridWidth.value}` : gridWidth.value;
 });
 
-gridHeigth.addEventListener("input", ()=>{
-    heigthValue.innerHTML = gridHeigth.value < 9 ? `0${gridHeigth.value}` : gridHeigth.value;
+gridHeight.addEventListener("input", ()=>{
+    heightValue.innerHTML = gridHeight.value < 9 ? `0${gridHeight.value}` : gridHeight.value;
 });
 
 window.onload = () => {
-    gridHeigth.value = 0;
+    gridHeight.value = 0;
     gridWidth.value = 0;
 };

62bd0206f8a6de9a6fa036ee1cecd05a79d4c5b5
Alcides Paz <alcidesnp@hotmail.com>
2024-06-24 19:22:42 -0300

    update: terminado o script e ajustes. Primeiros testes ok porém será necessário nova validação

--- Dia3-JS_text_editor/script.js
+++ Dia3-JS_text_editor/script.js
@@ -7,7 +7,7 @@ let linkButton = document.getElementById("createLink");
 let alignButtons = document.querySelectorAll(".align");
 let spacingButtons = document.querySelectorAll(".spacing");
 let formatButtons = document.querySelectorAll(".format");
-let scriptButtons = document.querySelectorAll(".script")
+let scriptButtons = document.querySelectorAll(".script");
 
 let fontList = [
     "Arial",
@@ -32,7 +32,7 @@ const initializer = () => {
         fontName.appendChild(option);
     });
 
-    for (let i = 1; i <=7; i++) {
+    for (let i = 1; i <= 7; i++) {
         let option = document.createElement("option");
         option.value = i;
         option.innerHTML = i;
@@ -54,17 +54,17 @@ optionsButtons.forEach((button) => {
 
 advancedOptionButton.forEach((button) => {
     button.addEventListener("change", () =>{
-        modifyText(button.id, false, null);
+        modifyText(button.id, false, button.value);
     });
 });
 
 linkButton.addEventListener("click", ()=> {
-    let userlink = prompt("Enter a URL?");
-    if(/http/i.test(userlink)){
-        modifyText(linkButton.id, false, userlink);
+    let userLink = prompt("Enter a URL?");
+    if(/http/i.test(userLink)){
+        modifyText(linkButton.id, false, userLink);
     } else {
-        userlink = "http://" + userlink;
-        modifyText(linkButton.id, false, userlink);
+        userLink = "http://" + userLink;
+        modifyText(linkButton.id, false, userLink);
     }
 });
 
